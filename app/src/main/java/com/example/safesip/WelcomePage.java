3d81d24386b9f10abfc343f619221c73ff441835
Baloul Sabrina <sabrina.baloul@student-cs.fr>
2025-11-17 14:07:45 +0100

    Welcome page + main activity button

--- app/src/main/java/com/example/safesip/WelcomePage.java
+++ app/src/main/java/com/example/safesip/WelcomePage.java
@@ -1,6 +1,9 @@
 package com.example.safesip;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +13,28 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class WelcomePage extends AppCompatActivity {
 
+
+    Button b1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        b1 = findViewById(R.id.button1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomePage.this, PersonalInformation.class);
+                startActivity(intent);
+            }
         });
+
+
+
+
+
     }
 }
