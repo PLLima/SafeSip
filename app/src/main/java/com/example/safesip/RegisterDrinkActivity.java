fa667d4134bc872101ed5554b124b1bff99c7446
Baloul Sabrina <sabrina.baloul@student-cs.fr>
2025-11-20 18:19:35 +0100

    feat : add aesthethical modifications such as padding to some texts

--- app/src/main/java/com/example/safesip/RegisterDrinkActivity.java
+++ app/src/main/java/com/example/safesip/RegisterDrinkActivity.java
@@ -171,7 +171,7 @@ public class RegisterDrinkActivity extends AppCompatActivity {
     private Button getButton(String drink, String volume, String percentageDrink) {
         Button drinkButton = new Button(this);
         drinkButton.setAllCaps(false);
-        drinkButton.setText(drink + " " + volume + " mL " + percentageDrink + "%");
+        drinkButton.setText(drink + " " + volume + " ml " + percentageDrink + "%");
         drinkButton.setOnClickListener(v -> {
             SharedPreferences dataBase = getSharedPreferences("history", Context.MODE_PRIVATE);
             String timesString = dataBase.getString("times", "");
